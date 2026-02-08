a4410d2d1353b2c391c9074902d6dba2b4e052be
Eliminator746 <90308790+Eliminator746@users.noreply.github.com>
2026-02-08 11:48:19 +0530

    Media folder created

--- server/server/settings.py
+++ server/server/settings.py
@@ -19,6 +19,10 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Upload_to (in models.py) is relative to MEDIA_ROOT, Django will now save files to: media/blog_images/ and media/profile_img/
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
